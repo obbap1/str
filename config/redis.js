abedf1d39d2271f76f70f975435018a1df8eb195
Obba Paschal <paschal.obba@stu.cu.edu.ng>
2019-07-01 18:20:41 +0100

    FIx: attach redis cloud url

--- config/redis.js
+++ config/redis.js
@@ -1,15 +1,15 @@
 const redis = require('redis');
 
-const url = 'redis://localhost:6379'; // process.env.REDIS_DB || 'redis://localhost:6379';
+const url = require('url'); 
+const redisURL = url.parse(process.env.REDISCLOUD_URL);
+// process.env.REDIS_DB || 'redis://localhost:6379';
 
 const Redis = (() => {
   let instance;
 
   function createInstance() {
-    const client = redis.createClient({ url });
-    client.select(0, (err) => {
-      if (err) throw err;
-    });
+    const client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+    client.auth(redisURL.auth.split(":")[1]);
     return client;
   }
 
